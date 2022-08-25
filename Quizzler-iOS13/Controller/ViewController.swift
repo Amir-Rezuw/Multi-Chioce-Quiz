700137f4bc4df897bdb3d74e7517a31dce733b30
AmirReza Jamali <amir7jamali.ar@yahoo.com>
2022-08-25 23:40:41 +0430

    redesign: leyout & buttons

--- Quizzler-iOS13/Controller/ViewController.swift
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -19,19 +19,31 @@ class ViewController: UIViewController {
     
     var quizBrain = QuizBrain()
     var scoreModel = ScoreModel()
+    
+    func changeButtonStyle(_ button:UIButton){
+        button.backgroundColor = UIColor.clear
+        button.layer.cornerRadius = 20
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor.init(red: 0, green: 209, blue: 223, alpha:1).cgColor
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         progressBar.progress = 0.0
         questionLabel.text = quizBrain.getQuestionText()
+        changeButtonStyle(falseButton)
+        changeButtonStyle(trueButton)
+        progressBar.clipsToBounds = true
+        progressBar.layer.cornerRadius = 5
     }
     @IBAction func buttonClicked(_ sender: UIButton) {
         
         let userAnswer = sender.currentTitle!.lowercased()
         let userGotItRight = quizBrain.checkAnswer(userAnswer)
         if(userGotItRight){
-            sender.backgroundColor = UIColor.green
+            sender.layer.borderColor = UIColor.green.cgColor
         } else {
-            sender.backgroundColor = UIColor.red
+            sender.layer.borderColor = UIColor.red.cgColor
         }
         
         scoreModel.updateUserScore(userGotItRight)
@@ -46,8 +58,8 @@ class ViewController: UIViewController {
     
     @objc func updateUI(){
         questionLabel.text = quizBrain.getQuestionText()
-        falseButton.backgroundColor = UIColor.clear
-        trueButton.backgroundColor = UIColor.clear
+        falseButton.layer.borderColor = UIColor.init(red: 0, green: 209, blue: 223, alpha:1).cgColor
+        trueButton.layer.borderColor = UIColor.init(red: 0, green: 209, blue: 223, alpha:1).cgColor
         progressBar.progress = quizBrain.getProgress()
     }
     
